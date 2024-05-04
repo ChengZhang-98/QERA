45f93f5d9462b4b616bbc6e45ad9ca14891f8aac
Cheng Zhang <chengzhang98@outlook.com>
2024-05-04 16:26:33 +0100

    support llm ptq baseline

--- src/loqer/pipeline.py
+++ src/loqer/pipeline.py
@@ -8,6 +8,8 @@ import torch
 from torch.utils.data import DataLoader
 import transformers
 from accelerate import dispatch_model
+from transformers import BitsAndBytesConfig, AwqConfig, GPTQConfig
+from auto_gptq import exllama_set_max_input_length
 
 from .statistic_profiler import register_scale_hooks
 from .datasets import get_data_module
@@ -119,6 +121,10 @@ def pipeline_loqer():
     disable_perplexity_eval = config["disable_perplexity_eval"]
     disable_lm_eval = config["disable_lm_eval"]
 
+    # check output directory
+    if output_dir is not None and output_dir.is_dir() and len(list(output_dir.iterdir())) > 0:
+        raise ValueError(f"Output directory {output_dir} is not empty")
+
     # Load model and tokenizer
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
     model = transformers.AutoModelForCausalLM.from_pretrained(
@@ -229,8 +235,6 @@ def pipeline_loqer():
 
     if output_dir is not None:
         logger.info(f"🚀 Saving results to {output_dir}")
-        if output_dir.is_dir() and len(list(output_dir.iterdir())) > 0:
-            raise ValueError(f"Output directory {output_dir} is not empty")
 
         output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -254,3 +258,305 @@ def pipeline_loqer():
         # save config
         with open(output_dir / "config.yaml", "w") as f:
             yaml.dump(config, f)
+
+
+def pipeline_fp16():
+    parser = ArgumentParser()
+    parser.add_argument("model_name", type=str, help="Model name")
+    parser.add_argument("--dtype", dest="dtype", type=str, help="Evaluation data type", default="float16")
+    parser.add_argument("--device-map", dest="device_map", type=str, help="Device map", default="auto-balance")
+    parser.add_argument("--num-workers", dest="num_workers", type=int, help="Number of workers", default=8)
+    parser.add_argument("--output-dir", dest="output_dir", type=str, help="Output directory", default=None)
+    parser.add_argument(
+        "--perplexity-eval-set",
+        dest="perplexity_eval_set",
+        type=str,
+        help="Perplexity evaluation set",
+        default="wikitext2",
+    )
+    parser.add_argument(
+        "--perplexity-eval-batch-size",
+        dest="perplexity_eval_batch_size",
+        type=int,
+        help="Perplexity evaluation batch size",
+        default=4,
+    )
+    parser.add_argument(
+        "--perplexity-max-seq-length",
+        dest="perplexity_max_seq_length",
+        type=int,
+        help="Perplexity max sequence length",
+        default=2048,
+    )
+    parser.add_argument(
+        "--lm-eval-tasks",
+        dest="lm_eval_tasks",
+        type=str,
+        nargs="+",
+        help="LM eval tasks",
+        default=[
+            "arc_easy",
+            "lambada_openai",
+            "piqa",
+            "winogrande",
+            "arc_challenge",
+            "boolq",
+            "openbookqa",
+        ],
+    )
+    parser.add_argument(
+        "--lm-eval-num-fewshot", dest="lm_eval_num_fewshot", type=int, help="LM eval num fewshot", default=0
+    )
+    parser.add_argument(
+        "--lm-eval-batch-size", dest="lm_eval_batch_size", type=int, help="LM eval batch size", default=16
+    )
+    parser.add_argument("--disable-perplexity-eval", dest="disable_perplexity_eval", action="store_true")
+    parser.add_argument("--disable-lm-eval", dest="disable_lm_eval", action="store_true")
+
+    args = parser.parse_args()
+
+    logger.info(f"Arguments: \n{pformat(vars(args), indent=4)}")
+
+    model_name = args.model_name
+    dtype = getattr(torch, args.dtype)
+    device_map = args.device_map
+    num_workers = args.num_workers
+    output_dir = Path(args.output_dir) if args.output_dir is not None else None
+    perplexity_evaluation_set = args.perplexity_eval_set
+    perplexity_eval_batch_size = args.perplexity_eval_batch_size
+    perplexity_max_seq_length = args.perplexity_max_seq_length
+    lm_eval_tasks = args.lm_eval_tasks
+    lm_eval_num_fewshot = args.lm_eval_num_fewshot
+    lm_eval_batch_size = args.lm_eval_batch_size
+    disable_perplexity_eval = args.disable_perplexity_eval
+    disable_lm_eval = args.disable_lm_eval
+
+    # check output directory
+    if output_dir is not None and output_dir.is_dir() and len(list(output_dir.iterdir())) > 0:
+        raise ValueError(f"Output directory {output_dir} is not empty")
+
+    # Load model and tokenizer
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=dtype)
+    model.eval()
+    data_collator = transformers.DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    if not disable_perplexity_eval:
+        logger.info("🚀 Evaluating perplexity...")
+        perplexity_datamodule = get_data_module(
+            name=perplexity_evaluation_set,
+            tokenizer=tokenizer,
+            padding="max_length",
+            max_length=perplexity_max_seq_length,
+            num_raw_samples=None,
+        )
+        perplexity_dataloader = DataLoader(
+            perplexity_datamodule["test"],
+            batch_size=perplexity_eval_batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            collate_fn=data_collator,
+        )
+
+        model = dispatch_model(model, device_map=create_device_map(model, device_map=device_map))
+        perplexity_results = evaluate_perplexity(
+            model=model,
+            eval_dataloader=perplexity_dataloader,
+            num_samples=None,
+            progress_bar=True,
+            input_device=None,
+            description="Evaluating perplexity",
+        )
+
+        logger.info(f"Perplexity: {perplexity_results['perplexity']:.4f}")
+
+    if not disable_lm_eval:
+        logger.info("🚀 Evaluating lm-eval downstream tasks...")
+        lm_eval_results = evaluate_harness_downstream(
+            model,
+            tasks=lm_eval_tasks,
+            num_fewshot=lm_eval_num_fewshot,
+            no_cache=True,
+            batch_size=lm_eval_batch_size,
+        )
+        logger.info(f"Downstream task results: \n{pformat(lm_eval_results)}")
+
+    if output_dir is not None:
+        logger.info(f"🚀 Saving results to {output_dir}")
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        # save perplexity results
+        if not disable_perplexity_eval:
+            with open(output_dir / "perplexity_results.yaml", "w") as f:
+                yaml.dump(perplexity_results, f)
+
+        # save lm-eval results
+        if not disable_lm_eval:
+            with open(output_dir / "lm_eval_results.yaml", "w") as f:
+                yaml.dump(lm_eval_results, f)
+
+        # save args
+        with open(output_dir / "args.yaml", "w") as f:
+            yaml.dump(vars(args), f)
+
+
+def pipeline_q_baseline():
+    parser = ArgumentParser()
+    parser.add_argument("model_name", type=str, help="Model name")
+    parser.add_argument("--load-in-4bit", dest="load_in_4bit", action="store_true", help="Load in 4-bit model")
+    parser.add_argument("--dtype", dest="dtype", type=str, help="Evaluation data type", default="float16")
+    parser.add_argument("--num-workers", dest="num_workers", type=int, help="Number of workers", default=8)
+    parser.add_argument("--output-dir", dest="output_dir", type=str, help="Output directory", default=None)
+    parser.add_argument(
+        "--perplexity-eval-set",
+        dest="perplexity_eval_set",
+        type=str,
+        help="Perplexity evaluation set",
+        default="wikitext2",
+    )
+    parser.add_argument(
+        "--perplexity-eval-batch-size",
+        dest="perplexity_eval_batch_size",
+        type=int,
+        help="Perplexity evaluation batch size",
+        default=4,
+    )
+    parser.add_argument(
+        "--perplexity-max-seq-length",
+        dest="perplexity_max_seq_length",
+        type=int,
+        help="Perplexity max sequence length",
+        default=2048,
+    )
+    parser.add_argument(
+        "--lm-eval-tasks",
+        dest="lm_eval_tasks",
+        type=str,
+        nargs="+",
+        help="LM eval tasks",
+        default=[
+            "arc_easy",
+            "lambada_openai",
+            "piqa",
+            "winogrande",
+            "arc_challenge",
+            "boolq",
+            "openbookqa",
+        ],
+    )
+    parser.add_argument(
+        "--lm-eval-num-fewshot", dest="lm_eval_num_fewshot", type=int, help="LM eval num fewshot", default=0
+    )
+    parser.add_argument(
+        "--lm-eval-batch-size", dest="lm_eval_batch_size", type=int, help="LM eval batch size", default=16
+    )
+    parser.add_argument("--disable-perplexity-eval", dest="disable_perplexity_eval", action="store_true")
+    parser.add_argument("--disable-lm-eval", dest="disable_lm_eval", action="store_true")
+
+    args = parser.parse_args()
+
+    logger.info(f"Arguments: \n{pformat(vars(args), indent=4)}")
+
+    model_name = args.model_name
+    load_in_4bit = args.load_in_4bit
+    dtype = getattr(torch, args.dtype)
+    num_workers = args.num_workers
+    output_dir = Path(args.output_dir) if args.output_dir is not None else None
+    perplexity_evaluation_set = args.perplexity_eval_set
+    perplexity_eval_batch_size = args.perplexity_eval_batch_size
+    perplexity_max_seq_length = args.perplexity_max_seq_length
+    lm_eval_tasks = args.lm_eval_tasks
+    lm_eval_num_fewshot = args.lm_eval_num_fewshot
+    lm_eval_batch_size = args.lm_eval_batch_size
+    disable_perplexity_eval = args.disable_perplexity_eval
+    disable_lm_eval = args.disable_lm_eval
+
+    # check output directory
+    if output_dir is not None and output_dir.is_dir() and len(list(output_dir.iterdir())) > 0:
+        raise ValueError(f"Output directory {output_dir} is not empty")
+
+    # Load model and tokenizer
+    if load_in_4bit:
+        quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_name, torch_dtype=dtype, quantization_config=quantization_config
+        )
+    else:
+        model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=dtype, device_map={"": 0})
+    model.eval()
+
+    if load_in_4bit:
+        q_method = "bnb-4bit"
+    else:
+        if isinstance(model.config.quantization_config, GPTQConfig):
+            q_method = "gptq"
+            model = exllama_set_max_input_length(model, 8192)
+            # model = dispatch_model(model, device_map=create_device_map(model, device_map="auto"))
+        elif isinstance(model.config.quantization_config, AwqConfig):
+            q_method = "awq"
+        elif isinstance(model.config.quantization_config, dict):
+            q_method = model.config.quantization_config["quant_method"]
+        else:
+            raise ValueError(f"Unknown quantization method: {model.config.quantization_config}")
+
+    logger.info(f"🔊 Quantization method: {q_method}")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    data_collator = transformers.DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    if not disable_perplexity_eval:
+        logger.info("🚀 Evaluating perplexity...")
+        perplexity_datamodule = get_data_module(
+            name=perplexity_evaluation_set,
+            tokenizer=tokenizer,
+            padding="max_length",
+            max_length=perplexity_max_seq_length,
+            num_raw_samples=None,
+        )
+        perplexity_dataloader = DataLoader(
+            perplexity_datamodule["test"],
+            batch_size=perplexity_eval_batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            collate_fn=data_collator,
+        )
+
+        model = dispatch_model(model, device_map=create_device_map(model, device_map=device_map))
+        perplexity_results = evaluate_perplexity(
+            model=model,
+            eval_dataloader=perplexity_dataloader,
+            num_samples=None,
+            progress_bar=True,
+            input_device=None,
+            description="Evaluating perplexity",
+        )
+
+        logger.info(f"Perplexity: {perplexity_results['perplexity']:.4f}")
+
+    if not disable_lm_eval:
+        logger.info("🚀 Evaluating lm-eval downstream tasks...")
+        lm_eval_results = evaluate_harness_downstream(
+            model,
+            tasks=lm_eval_tasks,
+            num_fewshot=lm_eval_num_fewshot,
+            no_cache=True,
+            batch_size=lm_eval_batch_size,
+        )
+        logger.info(f"Downstream task results: \n{pformat(lm_eval_results)}")
+
+    if output_dir is not None:
+        logger.info(f"🚀 Saving results to {output_dir}")
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        # save perplexity results
+        if not disable_perplexity_eval:
+            with open(output_dir / "perplexity_results.yaml", "w") as f:
+                yaml.dump(perplexity_results, f)
+
+        # save lm-eval results
+        if not disable_lm_eval:
+            with open(output_dir / "lm_eval_results.yaml", "w") as f:
+                yaml.dump(lm_eval_results, f)
+
+        # save args
+        with open(output_dir / "args.yaml", "w") as f:
+            yaml.dump(vars(args), f)
