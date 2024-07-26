77915cc4ded4469ea917b528eb058c4f9d86a785
Jeffrey <jefferywongtszhang@hotmail.com>
2024-07-26 22:33:16 +0100

    chore: add a chunked main for sulis

---
+++ main_chunked.py
@@ -0,0 +1,12 @@
+import sys
+from pathlib import Path
+
+sys.path.append(Path(__file__).parents[0].joinpath("src").as_posix())
+
+
+from loqer.pipeline import pipeline_loqer, pipeline_loqer_chunked
+from loqer.logging import set_logging_verbosity
+
+if __name__ == "__main__":
+    set_logging_verbosity("info")
+    pipeline_loqer_chunked()
