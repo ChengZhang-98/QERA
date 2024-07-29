e1bb579dabb8041acf55094de3e1a710471baf9f
Jeffrey <jefferywongtszhang@hotmail.com>
2024-07-29 20:06:37 +0100

    feat: add normalfloat quantizer

--- src/loqer/quantize/quantizers/__init__.py
+++ src/loqer/quantize/quantizers/__init__.py
@@ -3,7 +3,7 @@ from .integer import int_quantizer
 from .minifloat import minifloat_ieee_quantizer
 from .bypass import bypass_quantizer
 from .mxint import mxint_quantizer
-
+from .normalfloat import normalfloat_quantizer
 
 def get_quantizer(name: str):
     match name:
@@ -13,6 +13,8 @@ def get_quantizer(name: str):
             return block_fp_quantizer
         case "mxint":
             return mxint_quantizer
+        case "normalfloat":
+            return normalfloat_quantizer
         case "integer":
             return int_quantizer
         case "minifloat":
