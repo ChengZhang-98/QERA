divide lora_B by loraLinear.scaling; fix bugs

src/loqer/fine_tuning/initialization.py
@@ -106,6 +106,7 @@ def replace_lora_weights_loftq_4bit(
         ori_weight = safetensor_loader.get_tensor(name + ".weight")
 
         reduced_rank = module.r[adapter_name]
+        lora_layer_scaling = module.scaling[adapter_name]
 
         lora_A, lora_B, new_qweight, error_f_norm = init_lora_loftq_4bit(
             qweight=module.weight,
@@ -117,7 +118,7 @@ def replace_lora_weights_loftq_4bit(
         error_dict[name] = error_f_norm
 
         module.lora_A[adapter_name].weight.data = lora_A
-        module.lora_B[adapter_name].weight.data = lora_B
+        module.lora_B[adapter_name].weight.data = lora_B / lora_layer_scaling
         module.weight.data = new_qweight
 
     return error_dict
@@ -204,13 +205,16 @@ def replace_lora_weights_loftq_kbit(
     named_modules = {name: module for name, module in peft_model.named_modules()}
     error_dict = {}
 
-    for name, module in tqdm.tqdm(named_modules.items(), desc="Replacing LoRA adapters (Emulated 2-bit LoftQ)"):
+    for name, module in tqdm.tqdm(
+        named_modules.items(), desc=f"Replacing LoRA adapters (Emulated {num_bits}-bit LoftQ)"
+    ):
         if not isinstance(module, LoraLinear):
             continue
         if not name.startswith(prefix):
             raise TypeError(f"Not peft model")
 
         reduced_rank = module.r[adapter_name]
+        lora_layer_scaling = module.scaling[adapter_name]
 
         if quant_type in ["normal", "uniform"]:
             lora_A, lora_B, new_weight, error_f_norm = init_lora_loftq_2bit_bnb(
@@ -232,7 +236,7 @@ def replace_lora_weights_loftq_kbit(
         error_dict[name] = error_f_norm
 
         module.lora_A[adapter_name].weight.data = lora_A
-        module.lora_B[adapter_name].weight.data = lora_B
+        module.lora_B[adapter_name].weight.data = lora_B / lora_layer_scaling
         module.weight.data = new_weight
 
     return error_dict
@@ -250,9 +254,7 @@ def _low_rank_decomposition_loqer(x: torch.Tensor, reduced_rank: int):
     L = U @ torch.diag(S)[:, :reduced_rank]
     R = Vh[:reduced_rank, :]
 
-    error_f_norm = S[reduced_rank:].pow(2).sum().sqrt().cpu().item()
-
-    return L, R, error_f_norm
+    return L, R
 
 
 @torch.no_grad()
@@ -296,9 +298,11 @@ def init_lora_loqer_4bit(qweight, weight: torch.Tensor, scale: torch.Tensor, red
     residual_scaled = residual @ scale
 
     torch.cuda.empty_cache()
-    L, R_, error_f_norm = _low_rank_decomposition_loqer(residual_scaled, reduced_rank)
+    L, R_ = _low_rank_decomposition_loqer(residual_scaled, reduced_rank)
 
     R = torch.linalg.solve(scale, R_.T).T
+
+    error_f_norm = torch.linalg.norm(residual - L @ R_, ord="fro").cpu().item()
 
     lora_A, lora_B = R, L
     return lora_A, lora_B, qweight, error_f_norm
@@ -329,6 +333,7 @@ def replace_lora_weights_loqer_4bit(
         ori_weight = safetensor_loader.get_tensor(name + ".weight")
         scale = scale_dict[name]
         reduced_rank = module.r[adapter_name]
+        lora_layer_scaling = module.scaling[adapter_name]
 
         lora_A, lora_B, qweight, error_f_norm = init_lora_loqer_4bit(
             qweight=module.weight,
@@ -340,7 +345,7 @@ def replace_lora_weights_loqer_4bit(
         error_dict[name] = [error_f_norm]
 
         module.lora_A[adapter_name].weight.data = lora_A
-        module.lora_B[adapter_name].weight.data = lora_B
+        module.lora_B[adapter_name].weight.data = lora_B / lora_layer_scaling
         module.weight.data = qweight
 
     return error_dict
@@ -372,8 +377,10 @@ def init_lora_loqer_2bit_bnb(
     residual_scaled = residual @ scale
 
     torch.cuda.empty_cache()
-    L, R_, error_f_norm = _low_rank_decomposition_loqer(residual_scaled, reduced_rank)
+    L, R_ = _low_rank_decomposition_loqer(residual_scaled, reduced_rank)
     R = torch.linalg.solve(scale, R_.T).T
+
+    error_f_norm = torch.linalg.norm(residual - L @ R, ord="fro").cpu().item()
 
     lora_A, lora_B = R, L
     return lora_A, lora_B, dequantized_weight.to(orig_weight_dtype), error_f_norm
@@ -400,9 +407,10 @@ def init_lora_loqer_kbit_mxint(
     residual_scaled = residual @ scale
 
     torch.cuda.empty_cache()
-    L, R_, error_f_norm = _low_rank_decomposition_loqer(residual_scaled, reduced_rank)
-    R = torch.linalg.solve(scale, R_.T).T
+    L, R_ = _low_rank_decomposition_loqer(residual_scaled, reduced_rank)
+    R = torch.linalg.solve(scale, R_.T).T  # scale is symmetric, thus (S^-1).T = S^(-1)
 
+    error_f_norm = torch.linalg.norm(residual - L @ R, ord="fro").cpu().item()
     lora_A, lora_B = R, L
     return lora_A, lora_B, quantized_weight.to(orig_weight_dtype), error_f_norm
 
@@ -422,7 +430,9 @@ def replace_lora_weight_loqer_kbit(
     named_modules = {name: module for name, module in peft_model.named_modules()}
     error_dict = {}
 
-    for name, module in tqdm.tqdm(named_modules.items(), desc="Replacing LoRA adapters (Emulated 2-bit LoQER+)"):
+    for name, module in tqdm.tqdm(
+        named_modules.items(), desc=f"Replacing LoRA adapters (Emulated {num_bits}-bit LoQER+)"
+    ):
         if not isinstance(module, LoraLinear):
             continue
         if not name.startswith(prefix):
@@ -431,6 +441,7 @@ def replace_lora_weight_loqer_kbit(
         reduced_rank = module.r[adapter_name]
         name = name[len(prefix) :]
         scale = scale_dict[name]
+        lora_layer_scaling = module.scaling[adapter_name]
 
         if quant_type in ["nf", "fp"]:
             lora_A, lora_B, new_weight, error_f_norm = init_lora_loqer_2bit_bnb(
@@ -452,7 +463,7 @@ def replace_lora_weight_loqer_kbit(
         error_dict[name] = [error_f_norm]
 
         module.lora_A[adapter_name].weight.data = lora_A
-        module.lora_B[adapter_name].weight.data = lora_B
+        module.lora_B[adapter_name].weight.data = lora_B / lora_layer_scaling
         module.weight.data = new_weight
 
     return error_dict
@@ -503,7 +514,9 @@ def replace_lora_weight_qlora_kbit(peft_model, quant_type, num_bits: int, comput
     named_modules = {name: module for name, module in peft_model.named_modules()}
     error_dict = {}
 
-    for name, module in tqdm.tqdm(named_modules.items(), desc="Replacing LoRA adapters (Emulatd 2-bit qLoRA)"):
+    for name, module in tqdm.tqdm(
+        named_modules.items(), desc=f"Replacing LoRA adapters (Emulated {num_bits}-bit qLoRA)"
+    ):
         if not isinstance(module, LoraLinear):
             continue
         if not name.startswith(prefix):

src/loqer/models/roberta.py
@@ -2,10 +2,11 @@ import logging
 from copy import deepcopy
 
 from torch import nn
-from transformers.models.deberta_v2.modeling_deberta_v2 import (
-    DebertaV2Layer,
-    DebertaV2Encoder,
-    DebertaV2ForSequenceClassification,
+from transformers.models.roberta.modeling_roberta import (
+    RobertaLayer,
+    RobertaEncoder,
+    RobertaForSequenceClassification,
+    RobertaForMaskedLM,
 )
 
 from ..quantize import get_quantized_layer_cls
@@ -14,11 +15,11 @@ from ..utils import find_matched_pattern, get_layer_name, set_layer_by_name
 logger = logging.getLogger(__name__)
 
 
-def build_loqer_config_deberta_v2(model: DebertaV2ForSequenceClassification, loqer_config: dict):
-    assert isinstance(model, DebertaV2ForSequenceClassification)
+def build_loqer_config_roberta(model: RobertaForSequenceClassification, loqer_config: dict):
+    assert isinstance(model, (RobertaForSequenceClassification, RobertaForMaskedLM))
     parsed_config = {}
 
-    encoder: DebertaV2Encoder = model.deberta.encoder
+    encoder: RobertaEncoder = model.roberta.encoder
 
     for module in encoder.modules():
         if not isinstance(module, nn.Linear):
@@ -34,11 +35,11 @@ def build_loqer_config_deberta_v2(model: DebertaV2ForSequenceClassification, loq
     return parsed_config
 
 
-def quantize_deberta_v2(model: DebertaV2ForSequenceClassification, loqer_config: dict):
-    assert isinstance(model, DebertaV2ForSequenceClassification)
-    loqer_config = build_loqer_config_deberta_v2(model, loqer_config)
+def quantize_roberta(model: RobertaForSequenceClassification, loqer_config: dict):
+    assert isinstance(model, (RobertaForSequenceClassification, RobertaForMaskedLM))
+    loqer_config = build_loqer_config_roberta(model, loqer_config)
 
-    for module in model.deberta.encoder.modules():
+    for module in model.roberta.encoder.modules():
         if not isinstance(module, nn.Linear):
             continue
 
@@ -53,21 +54,21 @@ def quantize_deberta_v2(model: DebertaV2ForSequenceClassification, loqer_config:
         new_fc.load_state_dict(module.state_dict())
         set_layer_by_name(model, fc_name, new_fc)
 
-    model._no_split_modules = ["DebertaV2Layer"]
+    model._no_split_modules = ["RobertaLayer"]
 
     return model
 
 
-def find_layers_to_register_scale_hook_deberta_v2(model: DebertaV2ForSequenceClassification):
-    assert isinstance(model, DebertaV2ForSequenceClassification)
+def find_layers_to_register_scale_hook_roberta(model: RobertaForSequenceClassification):
+    assert isinstance(model, (RobertaForSequenceClassification, RobertaForMaskedLM))
     assert model.config._attn_implementation == "eager"
     layers_to_register = []
 
-    decoder_layer: DebertaV2Layer
-    for decoder_layer in model.deberta.encoder.layer:
-        k_name = get_layer_name(model, decoder_layer.attention.self.key_proj)
-        q_name = get_layer_name(model, decoder_layer.attention.self.query_proj)
-        v_name = get_layer_name(model, decoder_layer.attention.self.value_proj)
+    decoder_layer: RobertaLayer
+    for decoder_layer in model.roberta.encoder.layer:
+        k_name = get_layer_name(model, decoder_layer.attention.self.key)
+        q_name = get_layer_name(model, decoder_layer.attention.self.query)
+        v_name = get_layer_name(model, decoder_layer.attention.self.value)
         layers_to_register.append(dict(target_layer=k_name, layers_sharing_scale=[q_name, v_name]))
 
         o_name = get_layer_name(model, decoder_layer.attention.output.dense)
@@ -82,10 +83,10 @@ def find_layers_to_register_scale_hook_deberta_v2(model: DebertaV2ForSequenceCla
     return layers_to_register
 
 
-def find_layers_to_approximate_deberta_v2(model: DebertaV2ForSequenceClassification):
-    assert isinstance(model, DebertaV2ForSequenceClassification)
+def find_layers_to_approximate_roberta(model: RobertaForSequenceClassification):
+    assert isinstance(model, (RobertaForSequenceClassification, RobertaForMaskedLM))
     layers_to_approximate = []
-    for layer_name, layer in model.deberta.encoder.layer.named_modules():
+    for layer_name, layer in model.roberta.encoder.layer.named_modules():
         if not isinstance(layer, nn.Linear):
             continue
         layer_name = get_layer_name(model, layer)

src/loqer/peft_pipeline_v2.py
@@ -44,14 +44,14 @@ def adapt_and_save_clm_model(
     quant_bits: int,
     lora_rank: int,
     lora_alpha: float,
-    lora_target_modules: list[str],
+    lora_target_modules: list[str] | None,
     device_map: str,
     num_workers: int,
     overwrite_output_dir: bool,
     overwrite_dataset_cache: bool,
     loqer_scaling_mode: str,
     peek_post_init_metrics: bool,
-    lora_modules_to_save: list[str],
+    lora_modules_to_save: list[str] | None,  # lm_head will not be quantized
     mxint_block_size: int,
 ):
     """
@@ -66,6 +66,12 @@ def adapt_and_save_clm_model(
     assert loqer_scaling_mode in ["diag", "rxx"]
     if quant_type in ["nf", "fp"]:
         assert quant_bits in [2, 4]
+
+    if lora_target_modules is None:
+        lora_target_modules = "all-linear"
+        logger.warning(f" ⚠️ Defaulting lora_target_modules to {lora_target_modules}")
+    if lora_modules_to_save is None:
+        logger.warning(f" ⚠️ Defaulting lora_modules_to_save to 'None'")
 
     output_dir = Path(output_dir)
     if output_dir.exists():
@@ -141,7 +147,6 @@ def adapt_and_save_clm_model(
             bnb_4bit_use_double_quant=bnb_4bit_use_double_quant,
             bnb_4bit_quant_type=bnb_quant_type_4bit,
             # bnb_4bit_quant_storage=torch.bfloat16,
-            llm_int8_skip_modules=lora_modules_to_save,
         )
         model = transformers.AutoModelForCausalLM.from_pretrained(model_name_or_path, quantization_config=bnb_config)
     else:
@@ -252,6 +257,7 @@ def adapt_and_save_cls_model(
     adapter_init: str,
     output_dir: str,
     loqer_calibration_set: str,
+    loqer_calibration_set_type: str,
     loqer_num_calibration_samples: int,
     loqer_calibration_batch_size: int,
     loqer_max_seq_length: int,
@@ -260,14 +266,14 @@ def adapt_and_save_cls_model(
     quant_bits: int,
     lora_rank: int,
     lora_alpha: float,
-    lora_target_modules: list[str],
+    lora_target_modules: list[str] | None,
     device_map: str,
     num_workers: int,
     overwrite_output_dir: bool,
     overwrite_dataset_cache: bool,
     loqer_scaling_mode: str,
     peek_post_init_metrics: bool,
-    lora_modules_to_save: list[str],
+    lora_modules_to_save: list[str] | None,
     mxint_block_size: int,
 ):
     assert adapter_init in ["loftq", "loqer", "qlora", "lora"]
@@ -275,7 +281,27 @@ def adapt_and_save_cls_model(
     assert quant_type in ["nf", "fp", "mxint"]
     if quant_type in ["nf", "fp"]:
         assert quant_bits in [2, 4]
+    assert loqer_calibration_set_type in ["downstream", "pretrain"]
     PAD_TO_MAX_LENGTH = True
+    MLM_PROBABILITY = 0.15
+
+    if lora_target_modules is None:
+        if "deberta" in model_name_or_path.lower():
+            lora_target_modules = ["key_proj", "query_proj", "value_proj", "dense"]
+        elif "roberta" in model_name_or_path.lower():
+            lora_target_modules = r"roberta\.encoder\.layer\.\d+\.(attention\.self\.(query|key|value)|(attention\.output\.dense)|(intermediate\.dense)|(output\.dense))"
+        else:
+            raise ValueError(f"Cannot determine default modules to save for {model_name_or_path}")
+
+        logger.info(f"🔍 Using default lora_target_modules: {lora_target_modules}")
+
+    if lora_modules_to_save is None:
+        if "deberta" in model_name_or_path.lower():
+            lora_modules_to_save = ["pooler.dense", "classifier"]
+        elif "roberta" in model_name_or_path.lower():
+            lora_modules_to_save = ["classifier"]
+        else:
+            raise ValueError(f"Cannot determine default modules to save for {model_name_or_path}")
 
     output_dir = Path(output_dir)
     if output_dir.exists():
@@ -305,7 +331,12 @@ def adapt_and_save_cls_model(
             device_map = create_device_map(model, device_map)
             model = dispatch_model(model, device_map)
 
-        data_collator = transformers.default_data_collator
+        if loqer_calibration_set_type == "downstream":
+            data_collator = transformers.default_data_collator
+        else:
+            data_collator = transformers.DataCollatorForLanguageModeling(
+                tokenizer=tokenizer, mlm=True, mlm_probability=MLM_PROBABILITY
+            )
         if adapter_init == "loqer":
             layers_to_register_and_share = find_layers_to_register_scale_hook(model)
             profiler_factory = register_scale_hooks(
@@ -332,7 +363,7 @@ def adapt_and_save_cls_model(
         input_device = next(model.parameters()).device
         for i, batch in enumerate(calibration_dataloader):
             with torch.no_grad():
-                batch = {k: v.to(input_device) for k, v in batch.items()}
+                batch = {k: v.to(input_device) for k, v in batch.items() if k != "labels"}
                 outputs = model(**batch, output_hidden_states=True)
             last_hidden_states = outputs.hidden_states[-1]
             output_ref.append(last_hidden_states.cpu())
@@ -439,7 +470,7 @@ def adapt_and_save_cls_model(
         post_init_outputs = []
         input_device = next(peft_model.parameters()).device
         for i, batch in enumerate(calibration_dataloader):
-            batch = {k: v.to(input_device) for k, v in batch.items()}
+            batch = {k: v.to(input_device) for k, v in batch.items() if k != "labels"}
             with torch.no_grad():
                 outputs = peft_model(**batch, output_hidden_states=True)
             last_hidden_states = outputs.hidden_states[-1]
@@ -478,6 +509,13 @@ def adapt_and_save_pipeline():
     parser.add_argument("output_dir", type=str)
     parser.add_argument(
         "--loqer-calibration-set", type=str, default=None, help="Default: wikitext2_peft for clm, required for cls"
+    )
+    parser.add_argument(
+        "--loqer-calibration-set-type",
+        type=str,
+        default="downstream",
+        help="Default: downstream, required for cls",
+        choices=["downstream", "pretrain"],
     )
     parser.add_argument("--loqer-num-calibration-samples", type=int, default=128)
     parser.add_argument("--loqer-calibration-batch-size", type=int, default=2)
@@ -519,11 +557,6 @@ def adapt_and_save_pipeline():
     transformers.set_seed(args.seed)
 
     if args.model_type == "clm":
-        if args.lora_target_modules is None:
-            args.lora_target_modules = "all-linear"
-            logger.warning(f" ⚠️Defaulting lora_target_modules to {args.lora_target_modules}")
-        if args.lora_modules_to_save is None:
-            logger.warning(f" ⚠️Defaulting lora_modules_to_save to 'None'")
         adapt_and_save_clm_model(
             args.model_name_or_path,
             adapter_init=args.adapter_init,
@@ -548,17 +581,12 @@ def adapt_and_save_pipeline():
             mxint_block_size=args.mxint_block_size,
         )
     elif args.model_type == "cls":
-        if args.lora_target_modules is None:
-            args.lora_target_modules = ["key_proj", "query_proj", "value_proj", "dense"]
-            logger.warning(f"⚠️ Using default lora_target_modules: {args.lora_target_modules}")
-        if args.lora_modules_to_save is None:
-            args.lora_modules_to_save = ["pooler.dense", "classifier"]
-            logger.warning(f"⚠️ Using default lora_modules_to_save: {args.lora_modules_to_save}")
         adapt_and_save_cls_model(
             args.model_name_or_path,
             adapter_init=args.adapter_init,
             output_dir=args.output_dir,
             loqer_calibration_set=args.loqer_calibration_set,
+            loqer_calibration_set_type=args.loqer_calibration_set_type,
             loqer_num_calibration_samples=args.loqer_num_calibration_samples,
             loqer_calibration_batch_size=args.loqer_calibration_batch_size,
             loqer_max_seq_length=args.loqer_max_seq_length,