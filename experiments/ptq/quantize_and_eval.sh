9cb59aa4e61685fafbb136601cebf2c02a152448
Cheng Zhang <chengzhang98@outlook.com>
2025-02-04 00:14:18 +0000

    small ckpt

--- experiments/ptq/quantize_and_eval.sh
+++ experiments/ptq/quantize_and_eval.sh
@@ -11,7 +11,7 @@ cd ../../
 check_rval "cd to project root"
 
 scaling_mode="diag"
-model_name=meta-llama/Llama-3.1-8B
+model_name=TinyLlama/TinyLlama_v1.1 # change to other checkpoints like meta-llama/Llama-3.1-8B
 model_name_esc=$(echo $model_name | sed 's/\//-/g')
 
 if [[ $scaling_mode == "w-only" ]]; then
@@ -20,7 +20,7 @@ else
     other_args="--qera-scaling-mode $scaling_mode"
 fi
 
-HF_HUB_OFFLINE=1 python ptq_pipeline.py experiments/configs/w-only-uniform-rank.yaml \
+python ptq_pipeline.py experiments/configs/w-only-uniform-rank.yaml \
     --model-name $model_name \
     --perplexity-eval-batch-size 2 \
     --disable-perplexity-eval --lm-eval-num-fewshot 0 --lm-eval-batch-size auto \
